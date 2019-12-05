a3cb76947117b9654fb71bd83bc79da4f483f02d
Julian <cusco2891@gmail.com>
2019-12-05 00:24:59 -0500

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
+
     return (
+
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +18,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+
     );
   }
 }
