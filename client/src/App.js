448795c533e2e0f99587ba453d4f44cdcfc3204e
Julian <cusco2891@gmail.com>
2019-12-05 02:04:05 -0500

    main component page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "./pages/Main";
+import Profile from "./pages/Profile";
 
-import logo from "./logo.svg";
+
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -9,15 +12,24 @@ class App extends Component {
 
     return (
 
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+
+
+          {/* <Nav /> */}
+
+            <Switch>
+              <Route exact path="/" component={Main} />
+              <Route exact path="/profile" component={Profile} />
+             
+              {/* exact path = "*" component={unknown} */}
+
+            </Switch>
+          </div>
+
+          {/* <Footer /> */}
+
+      </Router>
 
     );
   }
---
+++ client/src/pages/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class Main extends Component {
+    render() {
+        return (
+
+            <h1>main page</h1>
+
+        );
+    }
+}
+
+export default Main;
