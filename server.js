dbfdede0a0ec1802f8a453886bf78b89a487ecdb
Julian <cusco2891@gmail.com>
2019-12-05 00:32:04 -0500

    server

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
+
+// const PORT = process.env.PORT || 3000;
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -15,9 +19,12 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+app.use(routes);
+
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
